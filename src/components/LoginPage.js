aed9c30f6ca46f72c7d00e0eef536c462d4fa02e
SreeBharathiR <bharathisree130@gmail.com>
2024-09-22 17:29:21 +0530

    commit

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { users } from "../userLogin";
 import { Link, useNavigate } from "react-router-dom";
 // import { AuthContext } from "../context/AuthContex";
